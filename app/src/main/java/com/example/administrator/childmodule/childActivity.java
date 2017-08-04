45706f98f2cd6ee418743624ade98ab810a05885
biaozhu <biaozhu@tujia.com>
2017-08-04 15:37:05 +0800

    first

---
+++ app/src/main/java/com/example/administrator/childmodule/childActivity.java
@@ -0,0 +1,13 @@
+package com.example.administrator.childmodule;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class childActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_child);
+    }
+}
